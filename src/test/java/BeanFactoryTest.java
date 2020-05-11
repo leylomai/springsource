5cabc17a562633b0470f398b7cc319dc6b25db1f
zhanlele <leylomai@foxmail.com>
2020-05-11 23:28:54 +0800

    spring source

---
+++ src/test/java/BeanFactoryTest.java
@@ -0,0 +1,16 @@
+import chapter2.MyTestBean;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class BeanFactoryTest {
+
+    @Test
+    public void testSimpleLoad() {
+        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+        MyTestBean bean = (MyTestBean) bf.getBean("myTestBean");
+        Assertions.assertEquals("testStr", bean.getTestStr());
+    }
+}
